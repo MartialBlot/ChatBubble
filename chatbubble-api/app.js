8dd53ce66ace3e16df188bad7e333acce3cfe65e
BLOT Martial <martial.blot@outlook.fr>
2019-09-29 04:10:55 +0200

    methode signup

---
+++ chatbubble-api/app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const server = express()
+const admin = require('./firebase-admin/admin')
+const router = express.Router()
+const db = admin.firestore();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+router.post('/signup/:userId', (req, res) => {
+    const data = req.body
+    const userName = req.params.userId
+    const docRef = db.collection("userProfiles").doc(userName)
+
+    let newInfos = docRef.set({
+        "mail" : data.mail,
+        "password" : data.password,
+    });
+})
+
+// router.get('/', (req, res) => {
+//     let path = db.collection('UserProfiles').doc('Name');
+//     let user = path.get()
+//         .then(doc => {
+//             if (!doc.exists) {
+//                 console.log('No user');
+//             } else {
+//                 console.log(doc.data());
+//                 res.send(doc.data())
+//             }
+//         })
+//         .catch(err => {
+//             console.log('Connection error', err);
+//         });
+// })
+
+
+
+
+
+module.exports = router
+
+
--- chatbubble-api/index.js
+++ chatbubble-api/index.js
@@ -2,6 +2,39 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 8000;
+
+const Router = require('./app');
+
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+async function verifyToken(req, res, next) {
+
+    const idToken = req.headers.authorization
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken)
+
+        if (decodedToken) {
+            req.body.uid = decodedToken.uid
+
+            return next()
+
+        } else {
+            return res.status(401).send('You are nor authorized ...')
+        }
+    } catch (e) {
+        return res.status(401).send('You are authorized !')
+    }
+}
+
+server.use('/api', Router)
+
 server.listen(PORT, () => {
     console.log(`Server Running on port: ${PORT}`);
 });
+
