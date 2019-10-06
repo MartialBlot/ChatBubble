00c00e43a86cf1862e00ce72c731717bc76022a1
nicolasvienot <nvienot@student.42.fr>
2019-10-07 01:49:55 +0200

    [NV] Contact visible tout de suite + messagenomessage

--- chatbubble-ui/src/Loading.js
+++ chatbubble-ui/src/Loading.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import styled from '@emotion/styled'
-
+import React from "react";
+import styled from "@emotion/styled";
 
 export const Loading = () => {
-
-    return <ProgressBar>
-        <LoadingText>
-            Loading ...
-        </LoadingText>
+  return (
+    <ProgressBar>
+      <LoadingText>Loading ...</LoadingText>
     </ProgressBar>
-}
+  );
+};
 
 const ProgressBar = styled.div`
   position: relative;
@@ -20,31 +18,31 @@ const ProgressBar = styled.div`
   display: flex;
   justify-content: center;
   overflow: hidden;
-   &:before{
-     content: '';
-     display: block;
-     position: absolute;
-     top: 0;
-     left: 0;
-     right: 0;
-     bottom: 0;
-     border-radius: 18px;
-     background-color: orangered;
-     animation: 2s popup linear;
-      @keyframes popup {
-        0% {
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 18px;
+    background-color: orangered;
+    animation: 2s popup linear;
+    @keyframes popup {
+      0% {
         transform: translateX(-200px);
       }
-        100% {
+      100% {
         transform: translateX(0);
+      }
     }
-   }
   }
-`
+`;
 
 const LoadingText = styled.span`
   font-weight: bold;
   font-size: 20px;
   text-align: center;
   position: relative;
-`
+`;
